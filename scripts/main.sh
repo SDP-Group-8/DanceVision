ca9f18254cc313de83a4e79c81a724f7be75812c
AlexWebb03 <alexanderwebb03@gmail.com>
2024-03-11 16:24:51 +0000

    update shell scripts

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,6 +1,7 @@
 stream_address=$1
 stream_port=$2
 turtle_address=$3
+no_ros=$4
 
 sh $DANCEVISION_SCRIPTS/startup_command_listener.sh $turtle_address &
-sh $DANCEVISION_SCRIPTS/startup_server.sh $stream_address $stream_port &
+sh $DANCEVISION_SCRIPTS/startup_server.sh $stream_address $stream_port $no_ros &
